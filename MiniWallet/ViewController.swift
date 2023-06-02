aad05c1708c075dda93e361d6030c58610ea0e7f
rejxcy <rejxcy0903@gmail.com>
2023-06-03 00:31:45 +0800

    project init

---
+++ MiniWallet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniWallet
+//
+//  Created by 胡志龍 on 2023/6/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
