c6d008abf93c6025d72b4e957a2ed4489129efd6
rejxcy <rejxcy0903@gmail.com>
2023-06-06 11:51:20 +0800

    設定 coreData 的資料結構

--- MiniWallet/Wallet/WalletCoreData/WalletItemOrder.swift
+++ MiniWallet/Wallet/WalletCoreData/WalletItemOrder.swift
@@ -1,1 +1,12 @@
 import Foundation
+import CoreData
+
+@objc(WalletItemOrder)
+public class WalletItemOrder: NSManagedObject, Identifiable {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<WalletItemOrder> {
+        return NSFetchRequest<WalletItemOrder>(entityName: "WalletItemOrder")
+    }
+
+    @NSManaged public var id: String
+    @NSManaged public var order: Int32
+}
